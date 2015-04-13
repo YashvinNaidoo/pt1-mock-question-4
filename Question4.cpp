ede2ab3e1075d85c7c02104d8bab86ac5cfd8f58
eugenol <eug@live.co.za>
2015-04-13 11:21:46 +0200

    add Animal abstract base class, add Bird and Dog derived classes

--- Question4.cpp
+++ Question4.cpp
@@ -11,3 +11,61 @@
 // 4.2 From class "Animal", derive a concrete class "Bird" and "Dog".  Print appropriate message for
 //     each method.
 // 4.3 Write a driver program which creates an object of each type and demonstrates POLYMORPHISM. 
+
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+	Animal();
+	~Animal();
+
+	virtual void name() = 0;
+	virtual void does() = 0;
+};
+
+class Bird : public Animal
+{
+public:
+	Bird();
+	~Bird();
+
+	virtual void name();
+	virtual void does();
+};
+
+class Dog : public Animal
+{
+public:
+	Dog();
+	~Dog();
+
+	virtual void name();
+	virtual void does();
+};
+
+void Bird::name()
+{
+	cout << "Bird" << endl;
+}
+
+void Bird::does()
+{
+	cout << "Chirps" << endl;
+}
+
+void Dog::name()
+{
+	cout << "Dog" << endl;
+}
+
+void Dog::does()
+{
+	cout << "Barks" << endl;
+}
+
+int main(int argc, char **argv)
+{
+
+}
