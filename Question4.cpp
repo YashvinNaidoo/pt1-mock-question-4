5707778732bc17b4af4e6e053b1254d398ab2923
eugenol <eug@live.co.za>
2015-04-13 12:12:20 +0200

    add test function

--- Question4.cpp
+++ Question4.cpp
@@ -15,31 +15,34 @@
 #include <iostream>
 using namespace std;
 
+// Animal Class - Abstract base class
 class Animal
 {
 public:
-	Animal();
-	~Animal();
+	Animal(){};
+	~Animal(){};
 
 	virtual void name() = 0;
 	virtual void does() = 0;
 };
 
+//Bird Derived from animal
 class Bird : public Animal
 {
 public:
-	Bird();
-	~Bird();
+	Bird(){};
+	~Bird(){};
 
 	virtual void name();
 	virtual void does();
 };
 
+//Dog Derived from animal
 class Dog : public Animal
 {
 public:
-	Dog();
-	~Dog();
+	Dog(){};
+	~Dog(){};
 
 	virtual void name();
 	virtual void does();
@@ -47,25 +50,45 @@ public:
 
 void Bird::name()
 {
-	cout << "Bird" << endl;
+	cout << "Bird";
 }
 
 void Bird::does()
 {
-	cout << "Chirps" << endl;
+	cout << "Chirps";
 }
 
 void Dog::name()
 {
-	cout << "Dog" << endl;
+	cout << "Dog";
 }
 
 void Dog::does()
 {
-	cout << "Barks" << endl;
+	cout << "Barks";
 }
 
 int main(int argc, char **argv)
 {
+	Animal *ptrAnimal[2];
+
+	ptrAnimal[0] = new Bird;
+	ptrAnimal[1] = new Dog;
+
+	for (int i = 0; i < 2; i++)
+	{
+		cout << "A ";
+		ptrAnimal[i]->name();
+		cout << " ";
+		ptrAnimal[i]->does();
+		cout << "." << endl;
+	}
+
+	for (int i = 0; i < 2; i++)
+	{
+		delete ptrAnimal[i];
+	}
+
+	return 0;
 
 }
